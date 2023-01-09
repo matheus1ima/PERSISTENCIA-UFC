b0bcb259b95e4e11c25c5639f57047e70f2560db
Matheus Lima <72159311+matheus1ima@users.noreply.github.com>
2023-01-09 18:30:08 -0300

    Create Atividade08

---
+++ Atividade08/README.md
@@ -0,0 +1,7 @@
+## Atividade
+
+### Lista de Exercícios 08 ###
+
+A Lista 08 consiste em complementar a Lista 07 com a implementação do DAO usando JPA.
+
+> 08.PERSISTÊNCIA
---
+++ Atividade08/src/DAOException.java
@@ -0,0 +1,18 @@
+public class DAOException extends RuntimeException {
+
+	public DAOException() {
+		super();
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+}
